2b9742d2bd3ba4d4980446ba8d9fa316cf2edec6
jfm-code <my.giangvu@gmail.com>
2024-10-25 16:08:23 -0400

    Design landing page component

--- ratemycourses-frontend/src/components/LandingPage.vue
+++ ratemycourses-frontend/src/components/LandingPage.vue
@@ -1,13 +1,40 @@
 <template>
-  <div>
-    <h1>Find My Courses</h1>
-    <h3>Please choose a CS course to see review</h3>
-    <!-- Prevent the default form submission behavior because we don't want the form to 
-    reload the page; instead, we want Vue Router to handle the navigation. -->
-    <form @submit.prevent="goToReview">
-      <input type="text" id="fname" name="fname"><br>
-      <input type="submit" value="Submit">
-    </form>
+  <div class="container">
+    <h1>Rate My Courses</h1>
+    <h2>Please choose an UML CS course to see review</h2>
+    <div class="course-list"> 
+      <div @click="goToReview" class="card">
+        <p>
+          <span>COMP.1020</span>
+          <span>Computing II</span>
+        </p>
+      </div>
+      <div @click="goToReview" class="card">
+        <p>
+          <span>COMP.1020</span>
+          <span>Computing II</span>
+        </p>
+      </div>
+      <div @click="goToReview" class="card">
+        <p>
+          <span>COMP.2010</span>
+          <span>Computing III</span>
+        </p>
+      </div>
+      <div @click="goToReview" class="card">
+        <p>
+          <span>COMP.2030</span>
+          <span>Assembly Prog. Language</span>
+        </p>
+      </div>
+      <div @click="goToReview" class="card">
+        <p>
+          <span>COMP.2040</span>
+          <span>Computing IV</span>
+        </p>
+      </div>
+      
+    </div>
   </div>
 </template>
 
@@ -28,5 +55,38 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.container {
+  background-image: linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('../umasslowell.jpg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center center;
+  height: 100vh;
+  background-attachment: fixed;
+  padding: 100px 0px 100px 0px;
+  color:white;
+}
+.course-list {
+  display: grid;
+  grid-template-columns: auto auto auto auto;
+  justify-self: center;
+}
+.card {
+  background-color: white;
+  padding:2px 10px 2px 10px; /* top, right, bottom, left */
+  margin-left: 20px;
+  margin-top: 30px;
+  margin: 30px 0px 30px 20px;
+  color: black;
+  user-select: none;
+  border-radius: 10px;
+  border-style: solid;
+  border-color:cadetblue;
+}
+.card:hover {
+  background-color:cadetblue;
+  color: white;
+}
+.card > p > span {
+  margin:5px;
+}
 </style>
