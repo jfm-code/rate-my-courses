f6af2652b0c9409852d5c019790017b4e31cac9a
William Nguyen <thangnguyen15700@gmail.com>
2024-10-25 15:17:28 -0400

    add simple UI for the ReviewPage component

--- ratemycourses-frontend/src/components/ReviewPage.vue
+++ ratemycourses-frontend/src/components/ReviewPage.vue
@@ -1,8 +1,21 @@
 <template>
+<div>
   <div>
-    <h1>Review Page</h1>
-    <p>This is the review page of each course.</p>
+    <h1>Computing I</h1>
+    <p>Average rating: 9/10</p>
   </div>
+  <div class="comment_sections">
+    <div id ="id of this comment from databse" class="comments">
+          <span class="rating">Rating: <span>7</span></span><br>
+          <div>some comments here</div>
+    </div>
+    <div id ="id2 of this comment from databse" class="comments">
+        <span class="rating">Rating: <span>6</span></span><br>
+        <div>some comments here</div>
+    </div>
+  </div>
+</div>
+
 </template>
 
 <script>
@@ -10,3 +23,31 @@ export default {
   name: 'ReviewPage',
 }
 </script>
+
+<style scoped>
+.comment_sections {
+    display: flex; /* Enables Flexbox */
+    justify-content: space-between; /* Distributes space between items */
+    justify-self: center;
+    flex-direction: column;
+     /* Space between items */
+    align-items: center; /* Centers items vertically */
+    width: 60vw;
+}
+.comments {
+  width: 100%;
+  height: auto;
+  background-color: #EFE8E8;
+  flex: 1; /* Items grow equally */
+  margin-top: 10px;
+  border-radius: 5px;
+}
+
+.rating {
+  background-color: #00FFFF;
+  padding: 4px;
+  border-radius: 6px;
+  margin-top: 10px;
+
+}
+</style>
