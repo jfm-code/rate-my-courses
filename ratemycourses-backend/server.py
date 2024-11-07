d1aaf3e0dd7dc9b154b3b84ada73bff00ecc0b12
William Nguyen <thangnguyen15700@gmail.com>
2024-11-07 15:43:17 -0500

    change to anly allow requests from the front-end

--- ratemycourses-backend/server.py
+++ ratemycourses-backend/server.py
@@ -2,10 +2,16 @@ from flask import Flask
 from flask_cors import CORS
 from routes.course_routes import course_bp
 from routes.review_routes import review_bp
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # initialize Flask app
 app = Flask(__name__)
-CORS(app)  # enable CORS for all routes
+CORS(app,  resources={r"/*": {"origins": os.getenv("FEDOMAIN")}})  # enable CORS for requests from the domain
+
+
 
 # Register blueprints
 # all course-related endpoints will be prefixed with /courses
