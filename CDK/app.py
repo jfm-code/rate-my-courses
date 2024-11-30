55117a9776ba825433ed1b08fee2d9f43f857791
William Nguyen <thangnguyen15700@gmail.com>
2024-11-30 18:59:01 -0500

    add creating the stack for amplify

--- CDK/app.py
+++ CDK/app.py
@@ -3,11 +3,12 @@ import os
 
 import aws_cdk as cdk
 
-from cdk.cdk_stack import RDS, EC2
+from cdk.cdk_stack import RDSStack, EC2Stack, AmplifyFrontendStack
 
 
 app = cdk.App()
-rds= RDS(app, "Group9RDS")
-ec2 = EC2(app, "Group9EC2", RDS=rds) # passing another into this stack allows stack cross reference. 
+rds= RDSStack(app, "Group9RDS")
+ec2 = EC2Stack(app, "Group9EC2", RDS=rds) # passing another stack into this stack allows stack cross reference. (accessing credentials of an instance )
+AmplifyFrontendStack(app, "AmplifyFrontendStack", ec2=ec2)
 
 app.synth()
